f3a17cf9443c618c1e71484bb283e39cce1e1e0e
Softlaptop <Softlaptop@DESKTOP-65SBLOR>
2026-04-05 21:14:35 +0200

    Exercise 1

---
+++ G-Net-40-ADV03/Collection Helper/CollectionHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_40_ADV03.Collection_Helper
+{
+    internal static class CollectionHelper
+    {
+        public static void PrintCollection<T>(string CollectionName, IEnumerable<T> items)
+        {
+            Console.WriteLine($"{CollectionName}: {string.Join(",", items)}");
+        }
+    }
+}
--- G-Net-40-ADV03/Program.cs
+++ G-Net-40-ADV03/Program.cs
@@ -1,10 +1,83 @@
-﻿namespace G_Net_40_ADV03
+﻿using System.Collections.Generic;
+using G_Net_40_ADV03.Collection_Helper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace G_Net_40_ADV03
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            #region Exercise 1
+
+            // Exercise 1: Student Grade Manager
+            // Create a program that manages student grades using One Of Collections
+
+            // 1) Create a Collection with these grades: 85, 92, 78, 95, 88, 70, 100, 65
+            // 2) Print the collection, Count, first and last grade
+            // 3) Sort the grades ascending, then print
+            // 4) Get the first grade above 90
+            // 5) Get all grades below 75(failing grades)
+            // 6) Remove all failing grades(below 75)
+            // 7) Check if any grade equals 100
+            // 8) Create a List<string> where each grade becomes "Grade: X"
+
+
+            //---------------------------------------------------------------------------------------------------------
+
+
+            // 1) Create a Collection with these grades: 85, 92, 78, 95, 88, 70, 100, 65
+            // List<int> grades = new List<int> { 85, 92, 78, 95, 88, 70, 100, 65 };
+
+            //----------------------------------------------------------------------------------------------------
+
+
+            // 2) Print the collection, Count, first and last grade
+            //Console.WriteLine($"Collection Count = { grades.Count}");
+            //Console.WriteLine($"First Grade = {grades[0]}");
+            //Console.WriteLine($"Last Grade = {grades[^1]}");
+
+            //----------------------------------------------------------------------------------------------------
+
+            // 3) Sort the grades ascending, then print
+            //grades.Sort();
+            //CollectionHelper.PrintCollection("Sorted Grades", grades);
+
+            //----------------------------------------------------------------------------------------------------
+
+
+            // 4) Get the first grade above 90
+            //int firstGradeAbove90 = grades.FirstOrDefault(g => g > 90);
+            //Console.WriteLine(firstGradeAbove90);
+
+            //----------------------------------------------------------------------------------------------------
+
+            // 5) Get all grades below 75(failing grades)
+            //List<int> gradesBelow75 = grades.FindAll(g => g < 75);
+            //CollectionHelper.PrintCollection("Grades Below(75) ", gradesBelow75);
+
+            //----------------------------------------------------------------------------------------------------
+
+            // 6) Remove all failing grades(below 75)
+            //List<int> failinggradesBelow75 = grades.FindAll(g => g < 75);
+            //grades.RemoveAll(g => g < 75);
+            //CollectionHelper.PrintCollection("Grades After Removing Failing Grades", grades);
+
+            //----------------------------------------------------------------------------------------------------
+
+            // 7) Check if any grade equals 100
+            //bool Check = grades.Exists(g => g == 100);
+            //Console.WriteLine(Check);
+
+            //----------------------------------------------------------------------------------------------------
+
+            // 8) Create a List<string> where each grade becomes "Grade: X"
+            //List<string> gradeStrings = grades.ConvertAll(x => $"Grade: {x}");
+            //CollectionHelper.PrintCollection("Grade Strings", gradeStrings);
+
+            #endregion
+
+
         }
     }
 }
